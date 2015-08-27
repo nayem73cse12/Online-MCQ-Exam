8aee5d403d58b22c5380cf94049e17a082da8381
Masiur Rahman Siddiki <masiur.sust@csesociety.org>
2015-08-27 23:46:42 +0600

    Name Changed

---
+++ quespage.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online MCQ Exam || Question Setter Panel </title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+  <h2>Form control: inline radio buttons</h2>
+  <p>The form below contains three inline radio buttons:</p>
+  <form role="form">
+    
+  </form>
+</div>
+
+</body>
+</html>
