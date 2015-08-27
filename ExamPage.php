b585dd7cf0b14c04da92635f529ecd4b17be761f
Abu Hanife <simplenayem@gmail.com>
2015-08-28 01:05:14 +0600

    Form is created with 5 question

---
+++ ExamPage.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Online MCQ Exam</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+</head>
+<body>
+
+
+
+
+<div class="container">
+
+<!--It is only for heading-->
+<form class="form-horizontal">
+  <fieldset>
+    <legend><h2>Online MCQ Exam</h2></legend>
+        </fieldset>
+      </form>
+
+
+<!--Starting of a question-->
+
+  <form role="form">
+    <div class="form-group">
+      <label for="email">Question 1:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter Your Question">
+    </div>
+    
+  </form>
+
+
+
+<form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="email">Option 1:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter option 1">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Option 2:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter option 2">
+    </div>
+    
+  </form>
+
+<br>
+
+  <form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="email">Option 3:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter option 3">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Option 4:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter option 4">
+    </div>
+    
+  </form>
+  <!--Finishing of a question-->
+
+<br>
+
+<!--Starting of a question-->
+
+  <form role="form">
+    <div class="form-group">
+      <label for="email">Question 2:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter Your Question">
+    </div>
+    
+  </form>
+
+
+
+<form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="email">Option 1:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter option 1">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Option 2:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter option 2">
+    </div>
+    
+  </form>
+
+<br>
+
+  <form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="email">Option 3:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter option 3">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Option 4:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter option 4">
+    </div>
+    
+  </form>
+  <!--Finishing of a question-->
+
+<br>
+
+
+<!--Starting of a question-->
+
+  <form role="form">
+    <div class="form-group">
+      <label for="email">Question 3:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter Your Question">
+    </div>
+    
+  </form>
+
+
+
+<form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="email">Option 1:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter option 1">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Option 2:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter option 2">
+    </div>
+    
+  </form>
+
+<br>
+
+  <form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="email">Option 3:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter option 3">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Option 4:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter option 4">
+    </div>
+    
+  </form>
+  <!--Finishing of a question-->
+
+<br>
+
+<!--Starting of a question-->
+
+  <form role="form">
+    <div class="form-group">
+      <label for="email">Question 4:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter Your Question">
+    </div>
+    
+  </form>
+
+
+
+<form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="email">Option 1:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter option 1">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Option 2:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter option 2">
+    </div>
+    
+  </form>
+
+<br>
+
+  <form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="email">Option 3:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter option 3">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Option 4:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter option 4">
+    </div>
+    
+  </form>
+  <!--Finishing of a question-->
+
+<br>
+
+  <!--Starting of a question-->
+
+  <form role="form">
+    <div class="form-group">
+      <label for="email">Question 5:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter Your Question">
+    </div>
+    
+  </form>
+
+
+
+<form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="email">Option 1:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter option 1">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Option 2:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter option 2">
+    </div>
+    
+  </form>
+
+<br>
+
+  <form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="email">Option 3:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter option 3">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Option 4:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter option 4">
+    </div>
+    
+  </form>
+  <!--Finishing of a question-->
+
+</div>
+
+
+
+</body>
+</html>
