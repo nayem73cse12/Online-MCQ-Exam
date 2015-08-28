eba9ecf4852138d23fcd647d76866f083f9ca28f
Abu Hanife <simplenayem@gmail.com>
2015-08-28 23:18:23 +0600

    Nothing else

--- ExamPage.php
+++ ExamPage.php
@@ -8,8 +8,231 @@
 </head>
 <body>
 
-<div class="page-header">
-  <h1>Online MCQ Exam</h1>
+
+
+
+<div class="container">
+
+<!--It is only for heading-->
+<form class="form-horizontal">
+  <fieldset>
+    <legend><h2>Online MCQ Exam</h2></legend>
+        </fieldset>
+      </form>
+
+
+<!--Starting of question no 1-->
+
+  <form role="form">
+    <div class="form-group">
+      <label for="question">Question 1:</label>
+      <input type="text" class="form-control" id="q1" placeholder="Enter Your Question">
+    </div>
+    
+  </form>
+
+
+
+<form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="option">Option 1:</label>
+      <input type="text" class="form-control" id="q1o1" placeholder="Enter option 1">
+    </div>
+    <div class="form-group">
+      <label for="option">Option 2:</label>
+      <input type="text" class="form-control" id="q1o2" placeholder="Enter option 2">
+    </div>
+    
+  </form>
+
+<br>
+
+  <form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="option">Option 3:</label>
+      <input type="text" class="form-control" id="q1o3" placeholder="Enter option 3">
+    </div>
+    <div class="form-group">
+      <label for="option">Option 4:</label>
+      <input type="text" class="form-control" id="q1o4" placeholder="Enter option 4">
+    </div>
+    
+  </form>
+
+ 
+
+  <!--Finishing of a question-->
+
+<br>
+
+<!--Starting of a question no 2-->
+
+  <form role="form">
+    <div class="form-group">
+      <label for="question">Question 1:</label>
+      <input type="text" class="form-control" id="q2" placeholder="Enter Your Question">
+    </div>
+    
+  </form>
+
+
+
+<form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="option">Option 1:</label>
+      <input type="text" class="form-control" id="q2o1" placeholder="Enter option 1">
+    </div>
+    <div class="form-group">
+      <label for="option">Option 2:</label>
+      <input type="text" class="form-control" id="q2o2" placeholder="Enter option 2">
+    </div>
+    
+  </form>
+
+<br>
+
+  <form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="option">Option 3:</label>
+      <input type="text" class="form-control" id="q2o3" placeholder="Enter option 3">
+    </div>
+    <div class="form-group">
+      <label for="option">Option 4:</label>
+      <input type="text" class="form-control" id="q2o4" placeholder="Enter option 4">
+    </div>
+    
+  </form>
+  <!--Finishing of a question-->
+<br>
+
+
+<!--Starting of question no 3-->
+
+  <form role="form">
+    <div class="form-group">
+      <label for="question">Question 1:</label>
+      <input type="text" class="form-control" id="q3" placeholder="Enter Your Question">
+    </div>
+    
+  </form>
+
+
+
+<form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="option">Option 1:</label>
+      <input type="text" class="form-control" id="q3o1" placeholder="Enter option 1">
+    </div>
+    <div class="form-group">
+      <label for="option">Option 2:</label>
+      <input type="text" class="form-control" id="q3o2" placeholder="Enter option 2">
+    </div>
+    
+  </form>
+
+<br>
+
+  <form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="option">Option 3:</label>
+      <input type="text" class="form-control" id="q3o3" placeholder="Enter option 3">
+    </div>
+    <div class="form-group">
+      <label for="option">Option 4:</label>
+      <input type="text" class="form-control" id="q3o4" placeholder="Enter option 4">
+    </div>
+    
+  </form>
+  <!--Finishing of a question-->
+<br>
+<!--Starting of question no 4-->
+
+  <form role="form">
+    <div class="form-group">
+      <label for="question">Question 1:</label>
+      <input type="text" class="form-control" id="q4" placeholder="Enter Your Question">
+    </div>
+    
+  </form>
+
+
+
+<form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="option">Option 1:</label>
+      <input type="text" class="form-control" id="q4o1" placeholder="Enter option 1">
+    </div>
+    <div class="form-group">
+      <label for="option">Option 2:</label>
+      <input type="text" class="form-control" id="q4o2" placeholder="Enter option 2">
+    </div>
+    
+  </form>
+
+<br>
+
+  <form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="option">Option 3:</label>
+      <input type="text" class="form-control" id="q4o3" placeholder="Enter option 3">
+    </div>
+    <div class="form-group">
+      <label for="option">Option 4:</label>
+      <input type="text" class="form-control" id="q4o4" placeholder="Enter option 4">
+    </div>
+    
+  </form>
+  <!--Finishing of a question-->
+
+<br>
+<!--Starting of question no 5-->
+
+  <form role="form">
+    <div class="form-group">
+      <label for="question">Question 1:</label>
+      <input type="text" class="form-control" id="q5" placeholder="Enter Your Question">
+    </div>
+    
+  </form>
+
+
+
+<form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="option">Option 1:</label>
+      <input type="text" class="form-control" id="q5o1" placeholder="Enter option 1">
+    </div>
+    <div class="form-group">
+      <label for="option">Option 2:</label>
+      <input type="text" class="form-control" id="q5o2" placeholder="Enter option 2">
+    </div>
+    
+  </form>
+
+<br>
+
+  <form class="form-inline" role="form">
+ 
+    <div class="form-group">
+      <label for="option">Option 3:</label>
+      <input type="text" class="form-control" id="q5o3" placeholder="Enter option 3">
+    </div>
+    <div class="form-group">
+      <label for="option">Option 4:</label>
+      <input type="text" class="form-control" id="q5o4" placeholder="Enter option 4">
+    </div>
+    
+  </form>
+  <!--Finishing of a question-->
 </div>
 
 
