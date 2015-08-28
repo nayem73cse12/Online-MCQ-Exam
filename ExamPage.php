d326002bacd58aec18b71d8fdc365317424929a0
Abu Hanife <simplenayem@gmail.com>
2015-08-29 01:57:20 +0600

    Complete Form

--- ExamPage.php
+++ ExamPage.php
@@ -10,15 +10,14 @@
 
 
 
-
 <div class="container">
 
 <!--It is only for heading-->
-<form class="form-horizontal">
-  <fieldset>
-    <legend><h2>Online MCQ Exam</h2></legend>
-        </fieldset>
-      </form>
+
+  <div class="jumbotron">
+    <h1><center>Online MCQ Exam</center></h1>      
+    <p></p>      
+  </div>
 
 
 <!--Starting of question no 1-->
@@ -35,14 +34,21 @@
 
 <form class="form-inline" role="form">
  
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 1:</label>
       <input type="text" class="form-control" id="q1o1" placeholder="Enter option 1">
     </div>
+    <div class="checkbox">
+    <label><input type="checkbox">   </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 2:</label>
       <input type="text" class="form-control" id="q1o2" placeholder="Enter option 2">
     </div>
+    
     
   </form>
 
@@ -50,10 +56,17 @@
 
   <form class="form-inline" role="form">
  
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 3:</label>
       <input type="text" class="form-control" id="q1o3" placeholder="Enter option 3">
     </div>
+
+    <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 4:</label>
       <input type="text" class="form-control" id="q1o4" placeholder="Enter option 4">
@@ -65,13 +78,13 @@
 
   <!--Finishing of a question-->
 
-<br>
+<br><br>
 
 <!--Starting of a question no 2-->
 
   <form role="form">
     <div class="form-group">
-      <label for="question">Question 1:</label>
+      <label for="question">Question 2:</label>
       <input type="text" class="form-control" id="q2" placeholder="Enter Your Question">
     </div>
     
@@ -81,10 +94,17 @@
 
 <form class="form-inline" role="form">
  
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 1:</label>
       <input type="text" class="form-control" id="q2o1" placeholder="Enter option 1">
     </div>
+
+    <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 2:</label>
       <input type="text" class="form-control" id="q2o2" placeholder="Enter option 2">
@@ -96,10 +116,16 @@
 
   <form class="form-inline" role="form">
  
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 3:</label>
       <input type="text" class="form-control" id="q2o3" placeholder="Enter option 3">
     </div>
+    <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 4:</label>
       <input type="text" class="form-control" id="q2o4" placeholder="Enter option 4">
@@ -108,13 +134,13 @@
   </form>
   <!--Finishing of a question-->
 <br>
-
+<br>
 
 <!--Starting of question no 3-->
 
   <form role="form">
     <div class="form-group">
-      <label for="question">Question 1:</label>
+      <label for="question">Question 3:</label>
       <input type="text" class="form-control" id="q3" placeholder="Enter Your Question">
     </div>
     
@@ -124,10 +150,16 @@
 
 <form class="form-inline" role="form">
  
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 1:</label>
       <input type="text" class="form-control" id="q3o1" placeholder="Enter option 1">
     </div>
+    <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 2:</label>
       <input type="text" class="form-control" id="q3o2" placeholder="Enter option 2">
@@ -139,10 +171,17 @@
 
   <form class="form-inline" role="form">
  
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 3:</label>
       <input type="text" class="form-control" id="q3o3" placeholder="Enter option 3">
     </div>
+
+    <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 4:</label>
       <input type="text" class="form-control" id="q3o4" placeholder="Enter option 4">
@@ -150,12 +189,12 @@
     
   </form>
   <!--Finishing of a question-->
-<br>
+<br><br>
 <!--Starting of question no 4-->
 
   <form role="form">
     <div class="form-group">
-      <label for="question">Question 1:</label>
+      <label for="question">Question 4:</label>
       <input type="text" class="form-control" id="q4" placeholder="Enter Your Question">
     </div>
     
@@ -165,10 +204,17 @@
 
 <form class="form-inline" role="form">
  
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 1:</label>
       <input type="text" class="form-control" id="q4o1" placeholder="Enter option 1">
     </div>
+
+    <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 2:</label>
       <input type="text" class="form-control" id="q4o2" placeholder="Enter option 2">
@@ -179,11 +225,17 @@
 <br>
 
   <form class="form-inline" role="form">
- 
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 3:</label>
       <input type="text" class="form-control" id="q4o3" placeholder="Enter option 3">
     </div>
+
+    <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 4:</label>
       <input type="text" class="form-control" id="q4o4" placeholder="Enter option 4">
@@ -192,12 +244,12 @@
   </form>
   <!--Finishing of a question-->
 
-<br>
+<br><br>
 <!--Starting of question no 5-->
 
   <form role="form">
     <div class="form-group">
-      <label for="question">Question 1:</label>
+      <label for="question">Question 5:</label>
       <input type="text" class="form-control" id="q5" placeholder="Enter Your Question">
     </div>
     
@@ -206,11 +258,16 @@
 
 
 <form class="form-inline" role="form">
- 
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 1:</label>
       <input type="text" class="form-control" id="q5o1" placeholder="Enter option 1">
     </div>
+    <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 2:</label>
       <input type="text" class="form-control" id="q5o2" placeholder="Enter option 2">
@@ -221,11 +278,16 @@
 <br>
 
   <form class="form-inline" role="form">
- 
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 3:</label>
       <input type="text" class="form-control" id="q5o3" placeholder="Enter option 3">
     </div>
+    <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
     <div class="form-group">
       <label for="option">Option 4:</label>
       <input type="text" class="form-control" id="q5o4" placeholder="Enter option 4">
@@ -233,6 +295,297 @@
     
   </form>
   <!--Finishing of a question-->
+
+<br><br>
+  <!--Starting of question no 6-->
+
+  <form role="form">
+    <div class="form-group">
+      <label for="question">Question 6:</label>
+      <input type="text" class="form-control" id="q6" placeholder="Enter Your Question">
+    </div>
+    
+  </form>
+
+
+
+<form class="form-inline" role="form">
+ 
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 1:</label>
+      <input type="text" class="form-control" id="q6o1" placeholder="Enter option 1">
+    </div>
+    <div class="checkbox">
+    <label><input type="checkbox">   </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 2:</label>
+      <input type="text" class="form-control" id="q6o2" placeholder="Enter option 2">
+    </div>
+    
+    
+  </form>
+
+<br>
+<br>
+  <form class="form-inline" role="form">
+ 
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 3:</label>
+      <input type="text" class="form-control" id="q6o3" placeholder="Enter option 3">
+    </div>
+
+    <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 4:</label>
+      <input type="text" class="form-control" id="q6o4" placeholder="Enter option 4">
+    </div>
+    
+  </form>
+
+  <!--Finishing of a question-->
+<br><br>
+
+<!--Starting of question no 7-->
+
+  <form role="form">
+    <div class="form-group">
+      <label for="question">Question 7:</label>
+      <input type="text" class="form-control" id="q7" placeholder="Enter Your Question">
+    </div>
+    
+  </form>
+
+
+
+<form class="form-inline" role="form">
+ 
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 1:</label>
+      <input type="text" class="form-control" id="q7o1" placeholder="Enter option 1">
+    </div>
+    <div class="checkbox">
+    <label><input type="checkbox">   </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 2:</label>
+      <input type="text" class="form-control" id="q7o2" placeholder="Enter option 2">
+    </div>
+    
+    
+  </form>
+
+<br>
+
+  <form class="form-inline" role="form">
+ 
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 3:</label>
+      <input type="text" class="form-control" id="q7o3" placeholder="Enter option 3">
+    </div>
+
+    <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 4:</label>
+      <input type="text" class="form-control" id="q7o4" placeholder="Enter option 4">
+    </div>
+    
+  </form>
+
+
+  <!--Finishing of a question-->
+
+<br><br>
+  <!--Starting of question no 8-->
+
+  <form role="form">
+    <div class="form-group">
+      <label for="question">Question 8:</label>
+      <input type="text" class="form-control" id="q8" placeholder="Enter Your Question">
+    </div>
+    
+  </form>
+
+
+
+<form class="form-inline" role="form">
+ 
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 1:</label>
+      <input type="text" class="form-control" id="q8o1" placeholder="Enter option 1">
+    </div>
+    <div class="checkbox">
+    <label><input type="checkbox">   </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 2:</label>
+      <input type="text" class="form-control" id="q8o2" placeholder="Enter option 2">
+    </div>
+    
+    
+  </form>
+
+<br>
+
+  <form class="form-inline" role="form">
+ 
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 3:</label>
+      <input type="text" class="form-control" id="q8o3" placeholder="Enter option 3">
+    </div>
+
+    <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 4:</label>
+      <input type="text" class="form-control" id="q8o4" placeholder="Enter option 4">
+    </div>
+    
+  </form>
+
+ 
+
+  <!--Finishing of a question-->
+<br><br>
+  <!--Starting of question no 9-->
+
+  <form role="form">
+    <div class="form-group">
+      <label for="question">Question 9:</label>
+      <input type="text" class="form-control" id="q9" placeholder="Enter Your Question">
+    </div>
+    
+  </form>
+
+
+
+<form class="form-inline" role="form">
+ 
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 1:</label>
+      <input type="text" class="form-control" id="q9o1" placeholder="Enter option 1">
+    </div>
+    <div class="checkbox">
+    <label><input type="checkbox">   </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 2:</label>
+      <input type="text" class="form-control" id="q9o2" placeholder="Enter option 2">
+    </div>
+    
+    
+  </form>
+
+<br>
+
+  <form class="form-inline" role="form">
+ 
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 3:</label>
+      <input type="text" class="form-control" id="q9o3" placeholder="Enter option 3">
+    </div>
+
+    <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 4:</label>
+      <input type="text" class="form-control" id="q9o4" placeholder="Enter option 4">
+    </div>
+    
+  </form>
+
+ 
+
+  <!--Finishing of a question-->
+<br><br>
+<!--Starting of question no 10-->
+
+  <form role="form">
+    <div class="form-group">
+      <label for="question">Question 10:</label>
+      <input type="text" class="form-control" id="q10" placeholder="Enter Your Question">
+    </div>
+    
+  </form>
+
+
+
+<form class="form-inline" role="form">
+ 
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 1:</label>
+      <input type="text" class="form-control" id="q10o1" placeholder="Enter option 1">
+    </div>
+    <div class="checkbox">
+    <label><input type="checkbox">   </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 2:</label>
+      <input type="text" class="form-control" id="q10o2" placeholder="Enter option 2">
+    </div>
+    
+    
+  </form>
+
+<br>
+
+  <form class="form-inline" role="form">
+ 
+ <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 3:</label>
+      <input type="text" class="form-control" id="q10o3" placeholder="Enter option 3">
+    </div>
+
+    <div class="checkbox">
+    <label><input type="checkbox"> </label>
+  </div>
+    <div class="form-group">
+      <label for="option">Option 4:</label>
+      <input type="text" class="form-control" id="q10o4" placeholder="Enter option 4">
+    </div>
+    
+  </form>
+
+ 
+
+  <!--Finishing of a question-->
+
 </div>
 
 
