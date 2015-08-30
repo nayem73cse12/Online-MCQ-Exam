132a9996009e5665c0af1eab6e15a316d701468c
Abu Hanife <simplenayem@gmail.com>
2015-08-31 01:18:07 +0600

    newly done

--- ExamPage.php
+++ ExamPage.php
@@ -46,14 +46,203 @@
         <label for="ex2">Option 4:</label>
         <input class="form-control" id="q1o1" name="q1o4" type="text">
       </div>
+      <!--Question 1 finishing-->
+
+       <label></label>
+  
+       <!--Question 2 starting-->
+        <div>
+        <label for="q2">Question 2:</label>
+        <input class="form-control input-lg" id="q2" name="q2" type="text">
+      </div>
+      <label></label>
+     <div class="col-xs-3">
+     <input type="checkbox" value="A">
+        <label for="ex2">Option 1:</label>
+        <input class="form-control" id="q1o1" name="q1o1" type="text">
+      </div>
+      <div class="col-xs-3">
+      <input type="checkbox" value="B">
+        <label for="ex2">Option 2:</label>
+        <input class="form-control" id="q1o1" name="q1o2" type="text">
+      </div>
+      <div class="col-xs-3">
+       <input type="checkbox" value="C">
+        <label for="ex2">Option 3:</label>
+        <input class="form-control" id="q1o1" name="q1o3" type="text">
+      </div>
+
+      <div class="col-xs-3">
+      <input type="checkbox" value="D">
+        <label for="ex2">Option 4:</label>
+        <input class="form-control" id="q1o1" name="q1o4" type="text">
+      </div>
+      <!--Question 1 finishing-->
+
+       <label></label>
+  
+       <!--Question 1 starting-->
+        <div>
+        <label for="q1">Question 1:</label>
+        <input class="form-control input-lg" id="q1" name="q1" type="text">
+      </div>
+      <label></label>
+     <div class="col-xs-3">
+     <input type="checkbox" value="A">
+        <label for="ex2">Option 1:</label>
+        <input class="form-control" id="q1o1" name="q1o1" type="text">
+      </div>
+      <div class="col-xs-3">
+      <input type="checkbox" value="B">
+        <label for="ex2">Option 2:</label>
+        <input class="form-control" id="q1o1" name="q1o2" type="text">
+      </div>
+      <div class="col-xs-3">
+       <input type="checkbox" value="C">
+        <label for="ex2">Option 3:</label>
+        <input class="form-control" id="q1o1" name="q1o3" type="text">
+      </div>
+
+      <div class="col-xs-3">
+      <input type="checkbox" value="D">
+        <label for="ex2">Option 4:</label>
+        <input class="form-control" id="q1o1" name="q1o4" type="text">
+      </div>
+      <!--Question 1 finishing-->
+
+       <label></label>
+  
+       <!--Question 1 starting-->
+        <div>
+        <label for="q1">Question 1:</label>
+        <input class="form-control input-lg" id="q1" name="q1" type="text">
+      </div>
+      <label></label>
+     <div class="col-xs-3">
+     <input type="checkbox" value="A">
+        <label for="ex2">Option 1:</label>
+        <input class="form-control" id="q1o1" name="q1o1" type="text">
+      </div>
+      <div class="col-xs-3">
+      <input type="checkbox" value="B">
+        <label for="ex2">Option 2:</label>
+        <input class="form-control" id="q1o1" name="q1o2" type="text">
+      </div>
+      <div class="col-xs-3">
+       <input type="checkbox" value="C">
+        <label for="ex2">Option 3:</label>
+        <input class="form-control" id="q1o1" name="q1o3" type="text">
+      </div>
+
+      <div class="col-xs-3">
+      <input type="checkbox" value="D">
+        <label for="ex2">Option 4:</label>
+        <input class="form-control" id="q1o1" name="q1o4" type="text">
+      </div>
+      <!--Question 1 finishing-->
+
+
+       <label></label>
+  
+       <!--Question 1 starting-->
+        <div>
+        <label for="q1">Question 1:</label>
+        <input class="form-control input-lg" id="q1" name="q1" type="text">
+      </div>
+      <label></label>
+     <div class="col-xs-3">
+     <input type="checkbox" value="A">
+        <label for="ex2">Option 1:</label>
+        <input class="form-control" id="q1o1" name="q1o1" type="text">
+      </div>
+      <div class="col-xs-3">
+      <input type="checkbox" value="B">
+        <label for="ex2">Option 2:</label>
+        <input class="form-control" id="q1o1" name="q1o2" type="text">
+      </div>
+      <div class="col-xs-3">
+       <input type="checkbox" value="C">
+        <label for="ex2">Option 3:</label>
+        <input class="form-control" id="q1o1" name="q1o3" type="text">
+      </div>
+
+      <div class="col-xs-3">
+      <input type="checkbox" value="D">
+        <label for="ex2">Option 4:</label>
+        <input class="form-control" id="q1o1" name="q1o4" type="text">
+      </div>
+      <!--Question 1 finishing-->
+      
+
+
+       <label></label>
+  
+       <!--Question 1 starting-->
+        <div>
+        <label for="q1">Question 1:</label>
+        <input class="form-control input-lg" id="q1" name="q1" type="text">
+      </div>
+      <label></label>
+     <div class="col-xs-3">
+     <input type="checkbox" value="A">
+        <label for="ex2">Option 1:</label>
+        <input class="form-control" id="q1o1" name="q1o1" type="text">
+      </div>
+      <div class="col-xs-3">
+      <input type="checkbox" value="B">
+        <label for="ex2">Option 2:</label>
+        <input class="form-control" id="q1o1" name="q1o2" type="text">
+      </div>
+      <div class="col-xs-3">
+       <input type="checkbox" value="C">
+        <label for="ex2">Option 3:</label>
+        <input class="form-control" id="q1o1" name="q1o3" type="text">
+      </div>
+
+      <div class="col-xs-3">
+      <input type="checkbox" value="D">
+        <label for="ex2">Option 4:</label>
+        <input class="form-control" id="q1o1" name="q1o4" type="text">
+      </div>
+      <!--Question 1 finishing-->
+
+
+
+      <label></label>
+  
+       <!--Question 1 starting-->
+        <div>
+        <label for="q1">Question 1:</label>
+        <input class="form-control input-lg" id="q1" name="q1" type="text">
+      </div>
+      <label></label>
+     <div class="col-xs-3">
+     <input type="checkbox" value="A">
+        <label for="ex2">Option 1:</label>
+        <input class="form-control" id="q1o1" name="q1o1" type="text">
+      </div>
+      <div class="col-xs-3">
+      <input type="checkbox" value="B">
+        <label for="ex2">Option 2:</label>
+        <input class="form-control" id="q1o1" name="q1o2" type="text">
+      </div>
+      <div class="col-xs-3">
+       <input type="checkbox" value="C">
+        <label for="ex2">Option 3:</label>
+        <input class="form-control" id="q1o1" name="q1o3" type="text">
+      </div>
+
+      <div class="col-xs-3">
+      <input type="checkbox" value="D">
+        <label for="ex2">Option 4:</label>
+        <input class="form-control" id="q1o1" name="q1o4" type="text">
+      </div>
+      <!--Question 1 finishing-->
+
+
+
     </div>
-    
-    <!--Question 1 finishing-->
-
-    <label></label>
-
-
-
+   
   </form>
 </div>
 
